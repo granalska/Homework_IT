cf9cd585230456581f353b5ee7f72f70b64ff62f
kgrnl <granalska.ss@gmail.com>
2026-06-05 22:07:31 +0300

    HTTP GET Додали перевірку доступності вебсайтів через curl з обробкою http статусу та закинули готовий результат у лог файл

---
+++ Computer System/hw4/script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#сайти для перевірки
+a=("https://google.com" "https://facebook.com" "https://twitter.com")
+
+#файл логів
+b="log.txt"
+
+#очищаємо файл
+echo "" > $b
+
+#цикл по сайтах
+for i in ${a[@]}
+do
+  #перевірка через curl
+  c=$(curl -L -s -o /dev/null -w "%{http_code}" $i)
+
+  if [ $c == 200 ]
+  then
+    echo "<$i> is UP" >> $b
+  else
+    echo "<$i> is DOWN" >> $b
+  fi
+done
+
+echo "Готовий файл: $b"
